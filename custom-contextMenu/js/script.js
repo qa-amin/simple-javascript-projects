b94bd60c0c23d6c47bbcb0c88427b8b60c537613
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-09 18:18:59 +0330

    add contextmenu EventListener to display contextmenu

--- custom-contextMenu/js/script.js
+++ custom-contextMenu/js/script.js
@@ -1,1 +1,38 @@
-﻿
+﻿let bodyElm = document.querySelector("body");
+
+let contextMenu = document.querySelector(".context-menu");
+
+
+
+bodyElm.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+
+    contextMenu.style.display = "none";
+
+    
+    contextMenu.style.top= "200px";
+    contextMenu.style.top = `${event.pageY}px`;
+    contextMenu.style.left = `${event.pageX}px`;
+
+
+    contextMenu.style.display = "inline";
+
+    
+
+
+    
+
+})
+
+
+bodyElm.addEventListener("click", function(){
+    contextMenu.style.display= "none";
+})
+
+bodyElm.addEventListener("keydown", function(event){
+
+    if(event.key === "Escape"){
+        contextMenu.style.display= "none";
+    }
+    
+})
