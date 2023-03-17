2b7296a0c5f88277ccb408e535133d2dad5b7df4
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-17 23:52:44 +0330

    add EventListener function for Change Button

--- Temp-Conversion/js/script.js
+++ Temp-Conversion/js/script.js
@@ -1,1 +1,35 @@
-﻿
+﻿// select <span></span> with .C class
+let cElm = document.querySelector(".C");
+
+// select <span></span> with .F class
+let fElm = document.querySelector(".F");
+
+// select <input> with #converter id
+let inputElm = document.querySelector("#converter");
+
+// select Change Button with .changeButton class
+let changeBtn =document.querySelector(".changeButton");
+
+
+let flag = "C";
+changeBtn.addEventListener("click", function(event){
+
+    if (cElm.innerHTML === "°C"){
+        cElm.innerHTML = "°F";
+        fElm.innerHTML = "°C";
+        inputElm.setAttribute("placeholder","°F");
+        document.title = "SalzLearn| Js | °F to °C";
+        flag = "F";
+    }
+    else{
+        cElm.innerHTML = "°C";
+        fElm.innerHTML = "°F";
+        inputElm.setAttribute("placeholder","°C");
+        document.title = "SalzLearn| Js | °C to °F";
+        let flag = "C";
+    }
+
+})
+
+
+
